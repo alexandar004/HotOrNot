9be9f67f4a909673e916d11c75d2af411e917d08
Alexander <adamqnov07@gmail.com>
2022-03-28 15:35:03 +0300

    configurate

--- HotOrNot/app/src/main/java/com/example/hotornot/MainScreenFragment.kt
+++ HotOrNot/app/src/main/java/com/example/hotornot/MainScreenFragment.kt
@@ -6,13 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class SplashScreen : Fragment() {
+class MainScreenFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash_screen, container, false)
+        return inflater.inflate(R.layout.fragment_main_screen, container, false)
     }
 }
