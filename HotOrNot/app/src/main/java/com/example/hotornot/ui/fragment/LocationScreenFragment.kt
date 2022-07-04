b6d60304ad56390650181aa6e330273a00227ff0
Alexander <adamqnov07@gmail.com>
2022-07-04 14:52:00 +0300

    change d  private fun hasLocationEnabled(): Boolean {

--- HotOrNot/app/src/main/java/com/example/hotornot/ui/fragment/LocationScreenFragment.kt
+++ HotOrNot/app/src/main/java/com/example/hotornot/ui/fragment/LocationScreenFragment.kt
@@ -7,9 +7,13 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Color
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
 import android.location.LocationManager
 import android.net.Uri
 import android.os.Bundle
+import android.os.Looper
 import android.provider.Settings
 import android.text.Spannable
 import android.text.SpannableString
@@ -18,12 +22,19 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.location.LocationManagerCompat
 import androidx.fragment.app.Fragment
 import com.example.hotornot.BuildConfig
 import com.example.hotornot.R
 import com.example.hotornot.databinding.FragmentLocationScreenBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import java.util.*
+import kotlin.collections.ArrayList
 
 private const val START_PAINTED_INDEX = 6
 private const val END_PAINTED_INDEX = 8
@@ -31,6 +42,53 @@ private const val END_PAINTED_INDEX = 8
 class LocationScreen : Fragment() {
 
     private lateinit var binding: FragmentLocationScreenBinding
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private val locationRequestId = 100
+
+    private var locationCallback = object : LocationCallback() {
+        override fun onLocationResult(p0: LocationResult) {
+            var location: Location = p0.lastLocation
+
+            updateAddressUI(location)
+        }
+
+        private fun updateAddressUI(location: Location) {
+            var addressList = ArrayList<Address>()
+            val geocoder = Geocoder(requireContext(), Locale.getDefault())
+
+            addressList = geocoder.getFromLocation(location.latitude,
+                location.longitude,
+                1) as ArrayList<Address>
+        }
+    }
+
+    private fun getLocation() {
+        if (newCheckForLocationPermission()) {
+            updateLocation()
+        }
+    }
+
+    private fun updateLocation() {
+        val locationRequest = LocationRequest()
+        locationRequest.apply {
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            interval = 10000
+            fastestInterval = 5000
+        }
+
+        fusedLocationProviderClient = FusedLocationProviderClient(this.requireContext())
+        if (ActivityCompat.checkSelfPermission(requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest,
+            locationCallback,
+            Looper.myLooper())
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -56,19 +114,19 @@ class LocationScreen : Fragment() {
         binding.motivationText.text = spannableString
     }
 
-    private fun checkPermissionForLocation(context: Context) {
-        if ((ContextCompat.checkSelfPermission(
+    private fun checkPermissionForLocation(context: Context): Boolean {
+        return if ((ContextCompat.checkSelfPermission(
                 context,
                 Manifest.permission.ACCESS_FINE_LOCATION
             ) == PackageManager.PERMISSION_DENIED)
         ) {
             val permission = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
             requestPermissions(permission, 1)
-            Log.d("AAA", "PERMISSION_DENIED")
             isActiveLocation(requireContext())
+            true
         } else {
-            Log.d("AAA", "PERMISSION_ENABLED")
             repeatAlertDialog()
+            false
         }
     }
 
@@ -85,6 +143,7 @@ class LocationScreen : Fragment() {
     private fun clickBtnChangeConfirmation() {
         binding.btnChange.setOnClickListener {
             checkPermissionForLocation(this.requireContext())
+            getLocation()
         }
     }
 
@@ -93,7 +152,6 @@ class LocationScreen : Fragment() {
         permissions: Array<out String>,
         grantResults: IntArray,
     ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if ((ContextCompat.checkSelfPermission(
                 requireContext(),
                 Manifest.permission.ACCESS_FINE_LOCATION
@@ -111,8 +169,8 @@ class LocationScreen : Fragment() {
         val builder = AlertDialog.Builder(requireContext())
 
         builder.apply {
-            setMessage("Location permission is needed to track your location current location !")
-            setTitle("Permission required")
+            setMessage(getString(R.string.location_permission))
+            setTitle(getString(R.string.required_permission))
             setPositiveButton(getText(R.string.ok)) { _, _ ->
                 checkPermissionForLocation(requireContext())
             }
@@ -122,20 +180,26 @@ class LocationScreen : Fragment() {
         }
     }
 
+    private fun newCheckForLocationPermission(): Boolean {
+
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        )
+            return true
+
+        return false
+
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == 1) {
             if (isActiveLocation(requireContext())) {
                 showHappyFace()
             } else {
                 showSadFace()
             }
-        }
-    }
-
-    private fun checkForActiveLocation() {
-        if (!isActiveLocation(requireContext())) {
-
         }
     }
 
